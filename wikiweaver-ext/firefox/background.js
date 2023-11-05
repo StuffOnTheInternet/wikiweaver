5e479ee4417e0c3760a35ea76f97abb1879ffbf4
Gustav Arneving <gustav.arneving@telia.com>
2023-11-05 11:11:36 +0100

    User can store login info

--- wikiweaver-ext/firefox/background.js
+++ wikiweaver-ext/firefox/background.js
@@ -1,18 +1,23 @@
 chrome.webNavigation.onCommitted.addListener(
-  async (event) => {
-    if (event.url.includes("#")) return;
-    if (event.transitionType == "reload") return;
+    async (event) => {
+        if (event.url.includes("#")) return;
+        if (event.transitionType == "reload") return;
 
-    const page = event.url.split("wiki/")[1].split("#")[0].replace(/_/g, " ");
-    const response = await fetch("http://localhost:4242/api/ext/page", {
-      method: "POST",
-      mode: "no-cors",
-      body: JSON.stringify({
-        lobby: "Bleh",
-        username: "Red",
-        page: page,
-      }),
-    });
-  },
-  { url: [{ hostSuffix: ".wikipedia.org" }] }
+        const options = await chrome.storage.local.get();
+        const page = event.url
+            .split("wiki/")[1]
+            .split("#")[0]
+            .replace(/_/g, " ");
+
+        const response = await fetch("http://localhost:4242/move", {
+            method: "POST",
+            mode: "no-cors",
+            body: JSON.stringify({
+                lobby: options.lobby,
+                username: options.username,
+                page: page,
+            }),
+        });
+    },
+    { url: [{ hostSuffix: ".wikipedia.org" }] }
 );
---
+++ wikiweaver-ext/popup/store_user.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("#connect_form");
+
+form.addEventListener("submit", (event) => {
+    chrome.storage.local.set({
+        username: form.input_username.value,
+        lobby: form.input_lobby.value,
+    });
+});
