9fc8306ea86b06f88c5826a6a5034f5f68d171bb
Victor Löfgren <victor.l0fgr3n@gmail.com>
2023-11-19 15:44:10 +0100

    ext: load js file at top of html, call init on dom loaded

--- wikiweaver-ext/popup/store_user.js
+++ wikiweaver-ext/popup/store_user.js
@@ -1,9 +1,13 @@
-const form = document.querySelector("#connect_form");
+async function init() {
+  const form = document.querySelector("#connect_form");
 
-form.addEventListener("submit", (event) => {
-  chrome.storage.local.set({
-    username: form.input_username.value,
-    lobby: form.input_lobby.value,
-    domain: form.input_domain.value,
+  form.addEventListener("submit", (event) => {
+    chrome.storage.local.set({
+      username: form.input_username.value,
+      lobby: form.input_lobby.value,
+      domain: form.input_domain.value,
+    });
   });
-});
+}
+
+document.addEventListener("DOMContentLoaded", () => init(), false);
