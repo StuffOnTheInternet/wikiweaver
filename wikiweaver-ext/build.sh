304de966d82f2f4d56726faefc0628d33d571710
Victor Löfgren <victor.l0fgr3n@gmail.com>
2024-02-03 17:16:47 +0100

    ext: minor script cleanup

--- wikiweaver-ext/build.sh
+++ wikiweaver-ext/build.sh
@@ -6,9 +6,11 @@ Build and package the WikiWeaver browser extension for Chrome or Firefox.
 
 Usage: $0 <chrome|firefox> [options]
 
+Example: $0 firefox --package v1.0.0
+
 Options:
-    -h, --help     Print this message and exit.
-    -p, --package  Also package the extension.
+    -h, --help              Print this message and exit.
+    -p, --package <version> Also package the extension.
 EOF
 }
 
@@ -39,7 +41,6 @@ while test $# -gt 0; do
 done
 
 if [[ -z $TARGET ]]; then
-    echo "No target specified. Must be either 'chrome' or 'firefox'"
     print_help_text
     exit 1
 fi
@@ -53,7 +54,7 @@ mkdir -p $BUILD_TARGET
 cp -r common/* $BUILD_TARGET
 cp -r $TARGET/* $BUILD_TARGET
 
-echo "Created extension in $BUILD_TARGET"
+echo "Extension files moved to $BUILD_TARGET"
 
 if [[ -n $PACKAGE_VERSION ]]; then
     PACKAGE_FILE_REL="$BUILD_BASE/wikiweaver-ext-$PACKAGE_VERSION-$TARGET.zip"
@@ -63,5 +64,5 @@ if [[ -n $PACKAGE_VERSION ]]; then
     PACKAGE_FILE_ABS="$PWD/$PACKAGE_FILE_REL"
     (cd $BUILD_TARGET && zip -qr $PACKAGE_FILE_ABS *)
 
-    echo "Packaged extension in $PACKAGE_FILE_REL"
+    echo "Extension files packaged into $PACKAGE_FILE_REL"
 fi
