0963f2a610175416acd75fceac962dfbbba3c900
Victor Löfgren <victor.lofgren99@pm.me>
2025-01-23 11:56:39 +0100

    Ask for permission instead of active tab
    Make our hostname the default host permission. Add all urls as optional
    permissions so that we can ask for any url dynamically. When a user
    saves a new url in the options, ask for host permissions on that
    particular url.
    
    Signed-off-by: Victor Löfgren <victor.lofgren99@pm.me>

--- wikiweaver-ext/options/options.js
+++ wikiweaver-ext/options/options.js
@@ -20,29 +20,41 @@ async function save(e) {
   const urlElem = document.querySelector("#url");
   const url = new URL(urlElem.value.toLowerCase() || urlElem.placeholder);
 
-  await Settings.local.Set("url", url.toString());
   await Settings.local.Set("autoOpenStartPage", document.querySelector("#auto-open-start-page").checked);
 
-  // TODO: show saved succeeded in some way
+  // For some reason keeping the port here made it so the script was not
+  // injected at localhost, when the server was set to http://localhost:3000
+  const port = url.port;
+  url.port = "";
+
+  const host = `${url.origin}/*`;
+
+  // We must request permissions as the first async function we call in this
+  // user input handler for it to work.
+  // See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/permissions/request
+  // and https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/User_actions
+  await chrome.permissions.request({ origins: [host] });
+
+  // What a mess...
+  url.port = port;
+  await Settings.local.Set("url", url.origin);
 
   if ((await chrome.scripting.getRegisteredContentScripts({ ids: ["join-lobby"] })).length > 0) {
     await chrome.scripting.unregisterContentScripts({ ids: ["join-lobby"] });
   }
 
-  // For some reason keeping the port here made it so the script was not
-  // injected at localhost, when the server was set to http://localhost:3000
-  url.port = "";
-
   const scripts = [
     {
       id: "join-lobby",
       js: ["/content/join-lobby.js"],
-      matches: [`${url.origin}/*`],
+      matches: [host],
     }
   ]
 
   await chrome.scripting.registerContentScripts(scripts);
+
+  // TODO: show saved succeeded in some way
 }
 
 document.addEventListener("DOMContentLoaded", () => init(), false);
-document.querySelector("form").addEventListener("submit", save);
+document.querySelector("#submit").addEventListener("click", save);
